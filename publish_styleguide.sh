a83ddbba089e5895b513b93c82eb9a12ed32058b
Keith Myerscough <Keith.Myerscough@sioux.eu>
2023-03-03 12:13:37 +0100

    Include -r to copy all the data.

--- publish_styleguide.sh
+++ publish_styleguide.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 git clone git@bitbucket.org:limebv/limebv.bitbucket.io.git
-cp docs/build/* limebv.bitbucket.io/JuliaStyleGuide.jl
+cp -r docs/build/* limebv.bitbucket.io/JuliaStyleGuide.jl
 cd limebv.bitbucket.io
 git config user.email "bitbucket@sioux.eu"
 git config user.name "Sioux Mathware Bitbucket Pipeline"
