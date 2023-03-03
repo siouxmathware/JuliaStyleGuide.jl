a19375b039efd2d59e018897e37307530be7eacb
Keith Myerscough <Keith.Myerscough@sioux.eu>
2023-03-03 11:56:11 +0100

    Fix cd into correct dir that should result in push to correct repo (if permissions allow)

--- publish_styleguide.sh
+++ publish_styleguide.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+set -e
 git clone git@bitbucket.org:limebv/limebv.bitbucket.io.git
-cp docs/build/* limebv.bitbucket.io.git/JuliaStyleGuide.jl
-cd limebv.bitbucket.io.git
+cp docs/build/* limebv.bitbucket.io/JuliaStyleGuide.jl
+cd limebv.bitbucket.io
 git config user.email "bitbucket@sioux.eu"
 git config user.name "Sioux Mathware Bitbucket Pipeline"
 git add --all
