1518ecbe5ac6e6dc4ca8a2856cf2a61f4b31f977
Keith Myerscough <Keith.Myerscough@sioux.eu>
2023-03-03 13:41:21 +0100

    Updated publish.

--- publish_styleguide.sh
+++ publish_styleguide.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-git clone https://x-token-auth:${ACCESS_TOKEN}@bitbucket.org:limebv/limebv.bitbucket.io.git
+git clone https://x-token-auth:${ACCESS_TOKEN}@bitbucket.org/limebv/limebv.bitbucket.io.git
 cp -r docs/build/* limebv.bitbucket.io/JuliaStyleGuide.jl
 cd limebv.bitbucket.io
-git config user.email "6401e36015d668edd8ee41a1@bots.bitbucket.org"
+git config user.email "6401e75a93cf2599462ea026@bots.bitbucket.org"
 git config user.name "Sioux Mathware Bitbucket Pipeline"
 git add --all
 git commit --allow-empty -m "Updated JuliaStyleGuide.jl from bitbucket"
 git push
+
