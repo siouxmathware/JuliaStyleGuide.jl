1542a2b01b5328258020de654791e5780dd14e68
Keith Myerscough <Keith.Myerscough@sioux.eu>
2023-03-03 13:15:20 +0100

    Change clone to use an access token.

--- publish_styleguide.sh
+++ publish_styleguide.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-git clone git@bitbucket.org:limebv/limebv.bitbucket.io.git
+git clone https://x-token-auth:${ACCESS_TOKEN}@bitbucket.org:limebv/limebv.bitbucket.io.git
 cp -r docs/build/* limebv.bitbucket.io/JuliaStyleGuide.jl
 cd limebv.bitbucket.io
-git config user.email "bitbucket@sioux.eu"
+git config user.email "6401e36015d668edd8ee41a1@bots.bitbucket.org"
 git config user.name "Sioux Mathware Bitbucket Pipeline"
 git add --all
 git commit --allow-empty -m "Updated JuliaStyleGuide.jl from bitbucket"
