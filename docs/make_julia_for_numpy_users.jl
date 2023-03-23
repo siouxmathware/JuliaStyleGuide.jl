4aa36baa5c8e54f844c26416caf03cd80b8d6d3c
Keith Myerscough <Keith.Myerscough@sioux.eu>
2023-03-23 11:15:31 +0100

    Add some rows on importing.

--- docs/make_julia_for_numpy_users.jl
+++ docs/make_julia_for_numpy_users.jl
@@ -1,11 +1,28 @@
 include("TableMaker.jl")
 using .TableMaker
-ct = TableMaker.ComparisonTable(
+general_purpose_table = TableMaker.ComparisonTable(
+    "# General Purpose Equivalents", 
     (p="Python", j="Julia", n="Note"), 
     (p="```python\n{}\n```", j="```julia\n{}\n```", n="{}"), 
     [
-        (p="import package", j="import Pkg", n="3"), 
-        (p="from package import *", j="using Pkg", n="c")
+        (p="import package", j="import Pkg", n="Import a package, requiring qualified names"), 
+        (p="from package import *", j="using Pkg", n="Import the contents of a package, populating your namespace"),
+        (p="from package import C, f", j="using Pkg: C, f", n="Import items from a package, populating your namespace"), 
+        (p="from package import C, f", j="import Pkg: C, f", n="Import items from a package, populating your namespace, _allows extending the functions with new methods_"), 
+        (p="from package import *", j="using Pkg", n="Import all _exported_ contents of a package, populating your namespace, Julia community standard, but not recommended"),
+        (p="info(func)\nhelp(func)\nfunc?  # IPython", j="?func", n="get help on the function f"), 
     ]
 )
-TableMaker.make_markdown(ct, joinpath("docs", "src", "julia_for_numpy_users.md"))
+linear_algebra_table = TableMaker.ComparisonTable(
+    "# Linear Algebra Equivalents", 
+    (p="Python", j="Julia", n="Note"), 
+    (p="```python\n{}\n```", j="```julia\n{}\n```", n="{}"), 
+    [
+        (p="import linalg from numpy", j="import LinearAlgebra as LA", n="Loading extra functionality"), 
+        (p="np.ndim(a)\na.ndim", j="ndims(a)", n="Number of dimensions of array `a`"),
+    ]
+)
+TableMaker.make_markdown(
+    [general_purpose_table, linear_algebra_table], 
+    joinpath("docs", "src", "julia_for_numpy_users.md")
+)
