a7030bc06d519f0bdee6e1b20158fc48a717c17a
Keith Myerscough <Keith.Myerscough@sioux.eu>
2023-03-23 20:21:17 +0100

    Included more examples.

--- docs/make_julia_for_numpy_users.jl
+++ docs/make_julia_for_numpy_users.jl
@@ -12,43 +12,92 @@ general_purpose_table = TableMaker.ComparisonTable(
         (p="from package import C, f", j="import Pkg: C, f", n="Import items from a package, populating your namespace, _allows extending the functions with new methods_"), 
         (p="from package import *", j="using Pkg", n="Import all _exported_ contents of a package, populating your namespace, Julia community standard, but not recommended"),
         (p="info(func)\nhelp(func)\nfunc?  # IPython", j="?func", n="get help on the function f"), 
+        (p="""
+for i in range(1, 4):
+    print(i)
+""",
+j = """
+for i in 1:3
+    println(i )
+end
+""",
+n="""
+use a for-loop to print the numbers 1, 2, and 3 using range
+"""),
+        (p="a and b", j="a && b", n="short-circuiting logical AND operator"),
+        (p="a or b", j="a || b", n="short-circuiting logical OR operator"),
+(p="""
+a = 4
+if a == 4:
+    print('a = 4')
+elif a == 5:
+    print('a = 5')
+""",
+j="""
+a = 4
+if a == 4
+    println(“a = 4”)
+elseif a == 5
+    print(“a = 5”)
+end
+""",
+n="""
+create an if-else statement to check if a is 4 or 5 and print result
+"""),
+        (p="1j", j="1im", n="complex numbers "),
+        (p="np.finfo(float).eps or np.spacing(1)", j="eps(Float64) or eps(1.0)", n="distance from 1 to the next larger representable real number in double precision"),
     ]
 )
+
 linear_algebra_table = TableMaker.ComparisonTable(
     "# Linear Algebra Equivalents", 
     (p="Python", j="Julia", n="Note"), 
     (p="```python\n{}\n```", j="```julia\n{}\n```", n="{}"), 
     [
-        (p="np.size(a) or a.size", j="length(a)", n="number of elements of array a"),
-        (p="np.shape(a) or a.shape", j="size(a)", n="“size” of array a"),
+        (p="from numpy import random\nfrom numpy import linalg as la", j="import Random\nimport LinearAlgebra as LA", n="imports that are used later on[^1]"),
+        (p="np.size(a)  # or\na.size", j="length(a)", n="number of elements of array a"),
+        (p="np.shape(a)  # or\na.shape", j="size(a)", n="“size” of array a"),
         (p="a.shape[n-1]", j="size(a)[n]", n="get the number of elements of the n-th dimension of array a."),
-        (p="np.array([[1., 2., 3.], [4., 5., 6.]])", j="[[1 2 3];[4 5 6]]", n="define a 2x3 2D array"),
+        (p="""
+np.array([
+    [1., 2., 3.], 
+    [4., 5., 6.]
+])
+""", 
+j="""[
+    [1 2 3];
+    [4 5 6]
+]""", 
+n="""
+define a 2x3 2D array
+"""),
         (p="np.block([[a, b], [c, d]])", j="[[a b]; [c d]]", n="construct a matrix from blocks a, b, c, and d"),
         (p="a[-1]", j="a[end]", n="access last element in 1D array a"),
         (p="a[1, 4]", j="a[2, 5]", n="access element in second row, fifth column in 2D array a"),
-        (p="a[1] or a[1, :]", j="a[2, :]", n="entire second row of 2D array a"),
-        (p="a[0:5] or a[:5] or a[0:5, :]", j="a[1:5, :]", n="first 5 rows of 2D array a"),
+        (p="a[1]  # or\na[1, :]", j="a[2, :]", n="entire second row of 2D array a"),
+        (p="a[0:5]  # or\na[:5]  # or\na[0:5, :]", j="a[1:5, :]", n="first 5 rows of 2D array a"),
         (p="a[-5:]", j="a[end-4:end, :]", n="last 5 rows of 2D array a"),
         (p="a[0:3, 4:9]", j="a[1:3, 5:9]", n="The first through third rows and fifth through ninth columns of a 2D array, a."),
         (p="a[2:21:2,:]", j="a[3:2:21, :]", n="every other row of a, starting with the third and going to the twenty-first"),
         (p="a[::2, :]", j="a[:2:end, :]", n="every other row of a, starting with the first"),
         (p="a[::-1,:]", j="a[end:-1:1, :]", n="a with rows in reverse order"),
-        (p="a.transpose() or a.T", j="transpose(a)", n="transpose of a"),
-        (p="a.conj().transpose() or a.conj().T", j="a’", n="conjugate transpose of a"),
+        (p="a.transpose()  # or\na.T", j="transpose(a)", n="transpose of a"),
+        (p="a.conj().transpose()  # or\na.conj().T", j="a’", n="conjugate transpose of a"),
         (p="a @ b", j="a*b", n="matrix multiply"),
         (p="a * b", j="a.*b", n="element-wise multiply"),
         (p="a/b", j="a./b", n="element-wise divide"),
         (p="a**3", j="a.^3", n="element-wise exponentiation "),
+        (p="la.matrixpower(a, 3)", j="a^3", n="matrix exponentiation "),
         (p="(a > 0.5)", j="x a .> 0.5", n="matrix whose i,jth element is (a_ij > 0.5)."),
         (p="np.nonzero(a > 0.5)", j="findall(ax .> 0.5)", n="find the indices where (a > 0.5)"),
-        (p="a[:,np.nonzero(v > 0.5)[0]]", j="a[:, LinearIndices(findall(v .> 0.5))]", n="extract the columns of a where vector v > 0.5"),
-        (p="a[:, v.T > 0.5]", j="a[:, LinearIndices(findall(v .> 0.5))] ", n="extract the columns of a where column vector v > 0.5"),
+        (p="a[:,np.nonzero(v > 0.5)[0]]", j="a[:, v .> 0.5]", n="extract the columns of a where vector v > 0.5"),
+        (p="a[:, v.T > 0.5]", j="a[:, v .> 0.5] ", n="extract the columns of a where column vector v > 0.5"),
         (p="a[a < 0.5]=0", j="a[a .< 0.5] .= 0", n="a with elements less than 0.5 zeroed out"),
         (p="a * (a > 0.5)", j="a .* (a .> 0.5)", n="a with elements less than 0.5 zeroed out"),
         (p="a[:] = 3", j="a .= 3", n="set all values to the same scalar value"),
         (p="y = x.copy()", j="y = copy(x)", n="Get copy"),
-        (p="y = x[1, :].copy()", j="y = x[1, :]", n="NumPy slices are by reference, Julia slices are by copy"),
-        (p="y = x[1, :]", j="@view y = x[1, :]", n="@view macro changes slices into Views (references)"),
+        (p="y = x[1, :].copy()", j="y = x[1, :]", n="numpy slices are by reference, Julia slices are by copy"),
+        (p="y = x[1, :]", j="@view y = x[1, :]", n="@view macro changes slices into views (references)"),
         (p="y = x.flatten()", j="x[:]", n="turn array into vector, note that Julia uses column-major order"),
         (p="np.arange(1., 11.)", j="1:10", n="create an increasing vector"),
         (p="np.arange(10.)", j="0:10", n="create an increasing vector"),
@@ -60,21 +109,22 @@ linear_algebra_table = TableMaker.ComparisonTable(
 np.eye(3)
 """,
 j="""
-UniformScaling(1) or 
-using LinearAlgebra; I
+UniformScaling(1)  # or
+LA.I  # from LinearAlgebra
 """,
 n="""
 3x3 identity matrix.
 """),
         (p="np.diag(a)", j="diag(a)", n="#3"),
-        (p="np.diag(v, 0)", j="Diagonal(a) or diagm(a)", n="#3"),
+        (p="np.diag(v, 0)", j="Diagonal(a)  # or\ndiagm(a)", n="#3"),
         (p="""
-from numpy.random import default_rng
+from numpy.random 
+    import default_rng
 
 rng = default_rng(42)
 rng.random(3, 4)
-"""),
-(p="""
+""",
+j="""
 using Random
 
 rng = Xoshiro(42)
@@ -117,7 +167,9 @@ n="""
 """),
         (p="np.tile(a, (m, n))", j="repeat(a, m, n)", n="create m by n copies of a"),
         (p="""
-np.concatenate((a,b),1) or np.hstack((a,b)) or np.column_stack((a,b))
+np.concatenate((a,b),1)  # or
+np.hstack((a,b))  # or
+np.column_stack((a,b))
 """,
 j="""
 hcat(a, b) or
@@ -128,7 +180,8 @@ n="""
 concatenate columns of a and b
 """),
 (p="""
-np.concatenate((a,b)) or np.vstack((a,b))
+np.concatenate((a,b))  # or
+np.vstack((a,b))
 """,
 j="""
 vcat(a, b) or
@@ -143,8 +196,7 @@ concatenate rows of a and b
 np.nanmax(a)
 """,
 j="""
-maximum(filter(!isnan, a)) or 
-maximum(x->isnan(x) ? -Inf : x, a)
+maximum(filter(!isnan, a))
 """,
 n="""
 maximum element of a, ignoring NaN.
@@ -154,11 +206,11 @@ Note that the second Julia option does not allocate an intermediate array.
         (p="a.max(1)", j="maximum(a;, dims=2)", n="maximum element of each row of array a"),
         (p="np.maximum(a, b)", j="max.(a, b)", n="compares a and b element-wise, and returns the maximum value from each pair"),
         (p="""
-np.sqrt(v @ v) or np.linalg.norm(v)
+np.sqrt(v @ v)  # or
+la.norm(v)
 """,
 j="""
-using LinearAlgebra
-norm(v)
+LA.norm(v)
 """,
 n="""
 L2 norm of vector v
@@ -167,68 +219,71 @@ L2 norm of vector v
         (p="np.logical_or(a,b)", j="a .|| b", n="element-by-element OR operator"),
         (p="a & b", j="a & b", n="bitwise AND operator"),
         (p="a | b", j="a | b", n="bitwise OR operator"),
-        (p="linalg.inv(a)", j="inv(a)", n="inverse of square 2D array a"),
+        (p="la.inv(a)", j="inv(a)", n="inverse of square 2D array a"),
         (p="""
-linalg.pinv(a)
+la.pinv(a)
 """,
 j="""
-using LinearAlgebra
-pinv(v)
+LA.pinv(v)
 """,
 n="""
 pseudo-inverse of 2D array a
 """),
 (p="""
-np.linalg.matrix_rank(a)
+la.matrix_rank(a)
 """,
 j="""
-using LinearAlgebra
-rank(v)
+LA.rank(v)
 """,
 n="""
 matrix rank of a 2D array a
 """),
-        (p="linalg.solve(a, b) if a is square; linalg.lstsq(a, b) otherwise", j="a \ b", n="solution of a x = b for x"),
         (p="""
-U, S, Vh = linalg.svd(a);
+la.solve(a, b)  # if square
+la.lstsq(a, b)  # otherwise
+""", 
+j="""
+a \\ b
+""", 
+n="""
+solution of a x = b for x
+"""),
+        (p="""
+U, S, Vh = la.svd(a);
 V = Vh.T
 """,
 j="""
-using LinearAlgebra
-F = svd(a)
+F = LA.svd(a)
 U, S, V = F; or
-Uonly, = svd(a)
+Uonly, = LA.svd(a)
 """,
 n="""
 singular value decomposition of a
 """),
 (p="""
-linalg.cholesky(a)
+la.cholesky(a)
 """,
 j="""
-using LinearAlgebra
-cholesky(a)
+LA.cholesky(a)
 """,
 n="""
 Cholesky factorization of a 2D array
 """),
 (p="""
-D,V = linalg.eig(a)
+D,V = la.eig(a)
 """,
 j="""
-using LinearAlgebra 
-F = eigen(a)
+F = LA.eigen(a)
 vals, vecs = F;
 """,
 n="""
 Eigenvalues λ and eigenvectors v of a, where av=λv
 """),
 (p="""
-D,V = linalg.eig(a, b)
+D,V = la.eig(a, b)
 """,
 j="""
-using LinearAlgebra 
-F = eigen(a, b)
+F = LA.eigen(a, b)
 vals, vecs = F;
 """,
 n="""
@@ -238,8 +293,7 @@ Eigenvalues λ and eigenvectors v of a, b; where av=λbv
 D,V = eigs(a, k=3)
 """,
 j="""
-using LinearAlgebra 
-F = eigen(a, rank(x)-2:rank(x))
+F = LA.eigen(a, rank(x)-2:rank(x))
 vals, vecs = F;
 """,
 n="""
@@ -248,7 +302,19 @@ find the k=3 largest eigenvalues and eigenvectors of 2D array, a
     ]
 )
 
+prologue = ""
+epilogue = """
+[^1]: These imports are used here in this way for two reasons: 
+(i) to allow short statements in the table and 
+(ii) to be explicit about the source of the methods imported.
+In proper code, neither the `la` in Python nor the `LA` in Julia would be advised. 
+For Julia, the `LinearAlgebra` would be more typically imported with a `using` statement, 
+perhaps _without_ specifiying the specific items. See ...
+"""
+
 TableMaker.make_markdown(
+    # prologue,
     [general_purpose_table, linear_algebra_table], 
+    # epilogue,
     joinpath("docs", "src", "julia_for_numpy_users.md")
 )
